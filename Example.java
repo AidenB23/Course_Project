c2856cf6ee9cab1c7eab00319d1a2a53fbfad909
AidenB23 <aidenjbryant05@gmail.com>
2025-11-17 21:16:35 -0800

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+/* Example.java
+ * Simple Java example added to the repository.
+ * To compile: javac Example.java
+ * To run: java Example
+ */
+public class Example {
+    public static void main(String[] args) {
+        
+    }
+}
